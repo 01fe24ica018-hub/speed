16f459e2ca345167ba2ef6a43579e11463d37f9a
sanjana <01fe24ica018@kletech.ac.in>
2025-11-28 11:26:05 +0530

    test_speed prgram

---
+++ test_speed.py
@@ -0,0 +1,12 @@
+from speed import calculate_speed
+import pytest
+
+def test_calculate_speed_normal():
+    assert calculate_speed(100,2) == 50
+
+def test_calculate_speed_fraction():
+    assert calculate_speed(50,0.5) == 100
+
+def test_calculate_speed_zero_time():
+    with pytest.raises(ZeroDivisionError):
+        calculate_speed(10,0)
