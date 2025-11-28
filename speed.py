e074981608ac4094ad761eb6c9c22b6841371ec8
sanjana <01fe24ica018@kletech.ac.in>
2025-11-28 10:34:10 +0530

    speed prgram

---
+++ speed.py
@@ -0,0 +1,20 @@
+import sys
+
+def calculate_speed(distance, time):
+    return distance / time
+
+if __name__ == "__main__":
+    # If command line arguments are given
+    if len(sys.argv) == 3:
+        distance = float(sys.argv[1])
+        time = float(sys.argv[2])
+    else:
+        # Else take user input
+        distance = float(input("Enter distance (km): "))
+        time = float(input("Enter time (hours): "))
+
+    if time == 0:
+        print("Time cannot be zero!")
+    else:
+        speed = calculate_speed(distance, time)
+        print(f"Speed = {speed} km/hr")
